a15ef058543807df0770fb41bdee5b59923a7a3c
Timos <psychoson91@gmail.com>
2026-07-21 19:32:00 +0300

    feat(filters): add owner filter dto

---
+++ MyVetApp/Core/Filters/OwnerFilterDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MyVetApp.Core.Filters
+{
+    public class OwnerFilterDTO
+    {
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
