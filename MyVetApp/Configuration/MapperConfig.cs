0fd048710f0c0990d3cace98ac4d38780c1c2a60
Timos <psychoson91@gmail.com>
2026-07-21 14:01:24 +0300

    feat(configuration): add object mappings for user, owner and pets

---
+++ MyVetApp/Configuration/MapperConfig.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MyVetApp.DTO;
+using MyVetApp.Models;
+
+namespace MyVetApp.Configuration
+{
+    public class MapperConfig : Profile
+    {
+
+        public MapperConfig()
+        {
+            CreateMap<User, UserReadOnlyDTO>()
+                .ForMember(dest => dest.UserRole, opt => opt.MapFrom(src => src.Role.Name));
+
+            CreateMap<OwnerSignupDTO, User>()
+                .ForMember(dest => dest.RoleId, opt => opt.MapFrom(src => src.RoleId!.Value));
+
+            CreateMap<OwnerSignupDTO, Owner>();
+
+            CreateMap<PetSignupDTO, Pet>();
+
+            CreateMap<Pet, PetReadOnlyDTO>();
+        }
+    }
+}
