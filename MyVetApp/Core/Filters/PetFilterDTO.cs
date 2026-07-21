35910ba0dbdf8219960ec4a48fef48838ba350e0
Timos <psychoson91@gmail.com>
2026-07-21 19:29:20 +0300

    feat(filters): add pet filter DTO with ownerId

---
+++ MyVetApp/Core/Filters/PetFilterDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace MyVetApp.Core.Filters
+{
+    public class PetFilterDTO
+    {
+        public string? Name { get; set; } 
+        public string? Species { get; set; }
+        public string? Gender { get; set; }
+        public bool? IsNeutered { get; set; }
+
+        public int? OwnerId { get; set; }
+    }
+}
