add350082e6ff5f60c8b84bac4f9702b45063b5f
Timos <psychoson91@gmail.com>
2026-07-22 18:22:25 +0300

    chore(DTO): add owner readonly dto

---
+++ MyVetApp/DTO/OwnerReadOnlyDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace MyVetApp.DTO
+{
+    public record OwnerReadOnlyDTO(
+        int Id,                 
+        string Firstname,        
+        string Lastname,        
+        string? Email,           
+        string? PhoneNumber      
+    );
+}
