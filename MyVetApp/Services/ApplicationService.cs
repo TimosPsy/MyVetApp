14dbaf13c2f07fa887b79b57fada5fb85506b6cd
Timos <psychoson91@gmail.com>
2026-07-22 19:45:19 +0300

    feat: add ApplicationService for dependency injection management

---
+++ MyVetApp/Services/ApplicationService.cs
@@ -0,0 +1,17 @@
+﻿namespace MyVetApp.Services
+{
+    public class ApplicationService : IApplicationService
+    {
+        public IUserService UserService { get; }
+        public IOwnerService OwnerService { get; }
+        public IPetService PetService { get; }
+
+        
+        public ApplicationService(IUserService userService, IOwnerService ownerService, IPetService petService)
+        { 
+            UserService = userService;
+            OwnerService = ownerService;
+            PetService = petService; 
+        }
+    }
+}
---
+++ MyVetApp/Services/IApplicationService.cs
@@ -0,0 +1,9 @@
+﻿namespace MyVetApp.Services
+{
+    public interface IApplicationService
+    {
+        IUserService UserService { get; }
+        IOwnerService OwnerService { get; }
+        IPetService PetService { get; } 
+    }
+}
