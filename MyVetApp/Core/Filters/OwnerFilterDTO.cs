1e68267c5faa9d428fd56eb99ac0dec23b0d8e38
Timos <psychoson91@gmail.com>
2026-07-22 19:37:41 +0300

    chore(core): add filters

--- MyVetApp/Core/Filters/OwnerFilterDTO.cs
+++ MyVetApp/Core/Filters/OwnerFilterDTO.cs
@@ -2,8 +2,6 @@
 {
     public class OwnerFilterDTO
     {
-        public string? Firstname { get; set; }
-        public string? Lastname { get; set; }
         public string? PhoneNumber { get; set; }
     }
 }
--- MyVetApp/Core/Filters/PetFilterDTO.cs
+++ MyVetApp/Core/Filters/PetFilterDTO.cs
@@ -6,7 +6,5 @@
         public string? Species { get; set; }
         public string? Gender { get; set; }
         public bool? IsNeutered { get; set; }
-
-        public int? OwnerId { get; set; }
     }
 }
