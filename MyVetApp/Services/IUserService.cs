a436cbc84a8a6e6dde80abab4ca1981970aa2d85
Timos <psychoson91@gmail.com>
2026-07-22 19:31:35 +0300

    feat(services): add user service contract impl

---
+++ MyVetApp/Services/IUserService.cs
@@ -0,0 +1,21 @@
+﻿using MyVetApp.Core;
+using MyVetApp.Core.Filters;
+using MyVetApp.DTO;
+using MyVetApp.Models;
+
+namespace MyVetApp.Services
+{
+    public interface IUserService
+    {
+        Task<User> VerifyAndGetUserAsync(UserLoginDTO credentials);
+
+        Task<UserReadOnlyDTO?> GetUserByUsernameAsync(string username);
+
+        Task<UserReadOnlyDTO> GetUserByIdAsync(int id);
+
+        Task<PaginatedResult<UserReadOnlyDTO>> GetPaginatedUsersFilteredAsync(
+            int pageNumber, int pageSize, UserFiltersDTO userFiltersDTO);
+
+        string CreateUserToken(User user);
+    }
+}
