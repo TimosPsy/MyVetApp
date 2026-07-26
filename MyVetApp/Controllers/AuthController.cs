84588d08e8174a625c37969659fb38236a819be9
Timos <psychoson91@gmail.com>
2026-07-26 20:57:32 +0300

    feat: add auth controller for register user and login

---
+++ MyVetApp/Controllers/AuthController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyVetApp.DTO;
+using MyVetApp.Exceptions;
+using MyVetApp.Services;
+
+namespace MyVetApp.Controllers
+{
+    [ApiController]
+    [Route("api/v1/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IApplicationService _applicationService;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IApplicationService applicationService, IConfiguration configuration)
+        {
+            _applicationService = applicationService;
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// Registers a new owner account.
+        /// </summary>
+        [HttpPost("register/owner")]
+        [AllowAnonymous]
+        [ProducesResponseType(typeof(UserReadOnlyDTO), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        public async Task<ActionResult<UserReadOnlyDTO>> RegisterOwner(
+            [FromBody] OwnerSignupDTO ownerSignupDTO)
+        {
+            var createdUser = await _applicationService.OwnerService
+                .SingUpOwnerUserAsync(ownerSignupDTO);
+
+            return CreatedAtAction(
+                actionName: nameof(UsersController.GetUserById),
+                controllerName: "Users",
+                routeValues: new { id = createdUser.Id },
+                value: createdUser);
+        }
+
+        /// <summary>
+        /// Authenticates a user and returns a JWT token.
+        /// </summary>
+        [HttpPost("login")]
+        [AllowAnonymous]
+        [ProducesResponseType(typeof(JwtTokenDTO), StatusCodes.Status200OK)]
+        public async Task<ActionResult<JwtTokenDTO>> Login(
+            [FromBody] UserLoginDTO credentials)
+        {
+            var user = await _applicationService.UserService
+                .VerifyAndGetUserAsync(credentials)
+                ?? throw new EntityNotAuthorizedException("User", "Bad Credentials");
+
+            var token = _applicationService.UserService.CreateUserToken(user);
+
+            return Ok(new JwtTokenDTO(token));
+        }
+    }
+}
