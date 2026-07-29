4b5f15c545b5c2b8d380f2145078c63aaab7650e
Timos <psychoson91@gmail.com>
2026-07-29 20:18:46 +0300

    feat: add petupdate dto

---
+++ MyVetApp/DTO/PetUpdateDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyVetApp.DTO
+{
+    public class PetUpdateDTO
+    {
+        [Required(ErrorMessage = "The {0} field is required.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "The {0} field is required.")]
+        public string? Species { get; set; }
+        
+        [Required(ErrorMessage = "The {0} field is required.")]
+        public string? Gender { get; set; }
+        
+        public bool IsNeutered { get; set; }
+        public double? Weight { get; set; }
+        public string? MicrochipNumber { get; set; }
+    }
+}
