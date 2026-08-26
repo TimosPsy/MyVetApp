005a1e284fbfe8fb9bcf9de1a8f829ea3a0fcdc3
Timos <psychoson91@gmail.com>
2026-08-26 19:34:44 +0300

    feat(routes): add RegisterStaffPage and update route structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,10 @@ import HomePage from "./pages/HomePage.tsx";
 import RegisterPage from "./pages/RegisterPage.tsx";
 import RegisterPetPage from "./pages/RegisterPetPage.tsx";
 import { UsersListPage } from "./pages/UserListPage.tsx";
-import PetListPage from "./pages/PetListPage.tsx";
+import {PetListPage} from "./pages/PetListPage.tsx";
+import UnauthorizedPage from "./pages/UnauthorizedPage.tsx";
+import  RegisterStaffPage  from "./pages/RegisterStaffPage.tsx";
+import ProfilePage from "./pages/ProfilePage.tsx";
 
 function App() {
   return (
@@ -17,21 +20,30 @@ function App() {
           
           <Route path="Login" element={<LoginPage />} />
           <Route path="register" element={<RegisterPage />} />
+          <Route path="register-staff" element={<RegisterStaffPage />} />
 
-          <Route path="users" element={<ProtectedRoute />}>
-            <Route index element={<UsersListPage />} />
+          <Route path="profile" element={<ProtectedRoute />}>
+            <Route index element={<ProfilePage />} />
           </Route>
 
+          <Route path="users" element={<ProtectedRoute requiredCapability="VIEW_USERS" />}>
+              <Route index element={<UsersListPage />} />
+          </Route>
+          
           <Route path="owners" element={<ProtectedRoute />}>
             <Route path=":ownerId/pets" element={<PetListPage />} />
             <Route path=":ownerId/pets/new" element={<RegisterPetPage />} />
             <Route path=":ownerId/pets/:petId" element={<RegisterPetPage />} />
           </Route>
 
-
-          <Route path="pets" element={<ProtectedRoute />}>
+          <Route 
+            path="pets" 
+            element={<ProtectedRoute requiredCapability="VIEW_PETS" />}
+          >
             <Route index element={<PetListPage />} />
           </Route>
+
+          <Route path="unauthorized" element={<UnauthorizedPage />} />
 
         </Route>
       </Routes>
